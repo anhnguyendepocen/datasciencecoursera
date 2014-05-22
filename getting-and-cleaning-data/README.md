4112fc1096bbe919c48d117f15371f5520173514
Mike Silva <mike.a.silva@gmail.com>
2014-05-22 14:40:24 -0400

    Committing changes for the getting and cleaning data class project

--- getting-and-cleaning-data/README.md
+++ getting-and-cleaning-data/README.md
@@ -9,35 +9,42 @@ run_analysis.R does the following:
 4. Appropriately labels the data set with descriptive activity names. 
 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-Data Layout
--------------------------
-
-![Data Layout](http://coursera-forum-screenshots.s3.amazonaws.com/d3/2e01f0dc7c11e390ad71b4be1de5b8/Slide2.png)
-
 How it Works
 -------------------------
-This script accomplishes the objectives outlined above by doing the 
-### Step 1: Get the data
-This script will create a directory for storing the data, download a zip file and extract the data for you if you don't have it on your local computer
-```{r}
-# Download the data
-if(!file.exists("UCI HAR Dataset.zip")){
-  url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(url,destfile="UCI HAR Dataset.zip")
-  
-  # This was designed for a Windows 7 environment.  If this doesn't work you 
-  # will need to uncomment the following line.
-  #download.file(url,destfile="UCI HAR Dataset.zip",method="curl")
-  
-  rm(url)  # Leave no trace
-}
+The script is broken into four large steps: 
 
-# Unzip the data
-if(!file.exists("UCI HAR Dataset/README.txt")){
-  unzip("UCI HAR Dataset.zip")
-}
+### Step 1. Get the Raw Data Files
+If the files do not exist in the user's working directory they are downloaded and unzipped.  The raw data will be in the "UCI HAR Dataset" directory found in the working directory.
+
+### Step 2. Combine & Transform the Raw Data Files
+The script combines 8 text files into a data frame called data.
+
+The following image show how the raw data files relate to the data object created by the run_analysis.R script:
+![Data Layout](http://coursera-forum-screenshots.s3.amazonaws.com/d3/2e01f0dc7c11e390ad71b4be1de5b8/Slide2.png)
+
+Underscores were removed from the activity labels (found in activity_labels.txt) and they were put into lower case letters.
+
+In naming the variables I followed the guidelines in the [Google R style guide](https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml).
+
+The variable names found in features.txt were "scrubbed" using the scrubber function found in run_analysis.R.  It creates human readable descriptive variable names.  For a full description of the variables I would refer you to the CodeBook.md file.
+
+Since we only wanted to extract the mean and the standard deviation variables, and they were denoted in features.txt by mean() and std(), the script identifies them using grep.  Variables with meanFreq in thier name were exclude.
+
+Once this step is complete data should have 10,299 observations and 69 variables.
+
+### Step 3. Create the Tidy Data Set
+Once the data was scrubbed, a tidy data set was created with the average of each variable for each activity and each subject.
+
+```{r}
+tidy.data <- aggregate(.~ subject.id + activity, data = data, FUN = mean)
 ```
-### Step 2: Import the text files
-```{r}
+The activity.id variable is dropped from the tidy data set.
 
+Once this step is complete tidy.data should have 180 observations and 68 variables.
+
+### Step 4. Export the Tidy Data Set
+Once this is accomplished the file is exported at a tab deliminated file.
+
+```{r}
+write.table(tidy.data, "tidydata.txt", sep="\t")
 ```
--- getting-and-cleaning-data/run_analysis.R
+++ getting-and-cleaning-data/run_analysis.R
@@ -5,8 +5,12 @@
 #
 ##
 
-# Step 1: Get the data
-# Download the data
+# This script has been designed to remove variables after they are no longer
+# needed.  If you want to keep the variables in the environment, set this
+# variable to FALSE.
+leave.no.trace = TRUE
+
+# Step 1: Get the Raw Data Files
 if(!file.exists("UCI HAR Dataset.zip")){
   url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(url,destfile="UCI HAR Dataset.zip")
@@ -15,8 +19,9 @@ if(!file.exists("UCI HAR Dataset.zip")){
   # will need to uncomment the following line.
   #download.file(url,destfile="UCI HAR Dataset.zip",method="curl")
   
-  # Leave no trace
-  rm(url)
+  if(leave.no.trace){
+    rm(url)
+  }
 }
 
 # Unzip the data
@@ -24,44 +29,52 @@ if(!file.exists("UCI HAR Dataset/README.txt")){
   unzip("UCI HAR Dataset.zip")
 }
 
-# Step 2: Combine the data
+# Step 2. Combine & Transform the Raw Data Files
+
+# Get the variable names
 features <- read.table("UCI HAR Dataset/features.txt", sep = " ", col.names = c("column.id", "messy.name"))
 
 # This function cleans up the messy variable names.  It inserts periods 
 # in between words, makes them lower case, and removes the () and -
-# characters
+# characters and makes the variable names easier to understand
 scrubber <- function(messy){
-  messy <- sub("Body",".body", messy)
-  messy <- sub("bodybody","body.body", messy)
-  messy <- sub("Gyro",".gyro", messy)
-  messy <- sub("Gravity",".gravity", messy)
-  messy <- sub("Jerk",".jerk", messy)
-  messy <- sub("Energy",".energy", messy)
-  messy <- sub("Mag", ".mag", messy)
+  messy <- sub("Body", ".body", messy)
+  messy <- sub("bodyBody", "body", messy)
+  messy <- sub("Gyro", ".gyroscope", messy)
+  messy <- sub("Gravity", ".gravity", messy)
+  messy <- sub("Jerk", ".jerk", messy)
+  messy <- sub("Energy", ".energy", messy)
+  messy <- sub("Mag", ".magnitude", messy)
   messy <- sub("arCoeff", "ar.coeff", messy)
-  messy <- sub("Acc",".acc", messy)
-  messy <- sub("\\()-",".", messy)
-  messy <- sub("\\()","", messy)
-  messy <- gsub("-",".", messy)
-  messy <- sub("\\(",".", messy)
-  messy <- sub("\\)","", messy)
-  messy <- sub("\\,",".", messy)
+  messy <- sub("Acc", ".accelerometer", messy)
+  messy <- sub("\\()-", ".", messy)
+  messy <- sub("\\()", "", messy)
+  messy <- gsub("-", ".", messy)
+  messy <- sub("\\(", ".", messy)
+  messy <- sub("\\)", "", messy)
+  messy <- sub("\\,", ".", messy)
   messy <- tolower(messy)
-  sub("bodybody","body", messy)
+  messy <- sub("t.body", "mean.time.body", messy)
+  messy <- sub("t.gravity", "mean.time.gravity", messy)
+  messy <- sub("f.body", "mean.frequency.body", messy)
+  messy <- sub("f.gravity", "mean.frequency.gravity", messy)
+  messy <- sub(".std", ".standard.deviation", messy)
+  return(messy)
 }
+
 # Now that the function is defined, let's scrub some names
 features$scrubbed.name <- scrubber(features$messy.name)
-features$keep <- (grepl("std|mean",features$messy.name) & !(grepl("meanFreq",features$messy.name)))
 
-# Leave no trace
-rm(scrubber)
+if(leave.no.trace){
+  rm(scrubber)
+}
 
+# Get the test and training data
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt", 
                       header = FALSE, 
                       col.names = features$scrubbed.name,
                       comment.char = "",
                       nrow=7352)
-
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt", 
                      header = FALSE, 
                      col.names = features$scrubbed.name,
@@ -70,27 +83,24 @@ X_test <- read.table("UCI HAR Dataset/test/X_test.txt",
 
 # Since we only want to keep the mean and standard deviation measures, let's
 # identify the columns we want to keep out of the 561 posibilities                          
+features$keep <- (grepl("std|mean",features$messy.name) & !(grepl("meanFreq",features$messy.name)))
 X_train <- X_train[,features$keep]
 X_test <- X_test[,features$keep]
 
-# Leave no trace
-rm(features)
-
-# This is just so I can track where the observations came from
-#X_test$measure = c("test")
-#X_train$measure = c("train")
+if(leave.no.trace){
+  rm(features)
+}
 
 # Add on the subject id
 subject_test <- read.delim("UCI HAR Dataset/test/subject_test.txt", sep = " ", header = FALSE, col.names = c("subject.id"))
-
 subject_train <- read.delim("UCI HAR Dataset/train/subject_train.txt", sep = " ", header = FALSE, col.names = c("subject.id"))
-
 X_test$subject.id <- subject_test$subject.id
 X_train$subject.id <- subject_train$subject.id
 
-# Leave no trace
-rm(subject_test)
-rm(subject_train)
+if(leave.no.trace){
+  rm(subject_test)
+  rm(subject_train)
+}
 
 # Add on the activity id
 Y_train <- read.table("UCI HAR Dataset/train/y_train.txt", 
@@ -104,45 +114,51 @@ Y_test <- read.table("UCI HAR Dataset/test/Y_test.txt",
                      col.names = c("activity.id"),
                      comment.char = "",
                      nrow=2947)
-
 X_test$activity.id <- Y_test$activity.id
 X_train$activity.id <- Y_train$activity.id
 
-# Leave no trace
-rm(Y_test)
-rm(Y_train)
+if(leave.no.trace){
+  rm(Y_test)
+  rm(Y_train)
+}
 
+# Combine the training and test data
 data <- rbind(X_train, X_test)
 
-# Leave no trace
-rm(X_test)
-rm(X_train)
+if(leave.no.trace){
+  rm(X_test)
+  rm(X_train)
+}
 
 # Add on the activity labels
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", sep = " ", col.names = c("activity.id", "activity.label"))
 # make the activity labels lower case and with a space instead of an underscore
 activity_labels$activity.label <- gsub("_"," ", tolower(activity_labels$activity.label))
+data$activity <- activity_labels[data$activity.id,2]
 
-data$activity.label <- activity_labels[data$activity.id,2]
-rm(activity_labels)  # Leave no trace
+if(leave.no.trace){
+  rm(activity_labels)
+}
 
-# data has 10,299 observations and 90 variables
+# data has 10,299 observations and 69 variables
 
-# Step 3: Do the analysis
-# This step invloced getting the average of each variable for each combination 
-# of subject and activity, and saving it to the tidy.data data frame
-data$activity <- as.factor(paste0("Subject ",data$subject.id,": ",data$activity.label))
+# Step 3. Create the Tidy Data Set
 
-# exclude variables subject.id, activity.id, activity.label
-drop <- names(data) %in% c("subject.id", "activity.id", "activity.label") 
+# Now that we have the data we need to get the average of each variable for 
+# each combination of subject and activity, and saving it to the tidy.data 
+# data frame
+tidy.data <- aggregate(.~ subject.id + activity, data = data, FUN = mean)
 
-# Tranform data frame into data table
-require(data.table)
-data <- as.data.table(data[!drop])
+# Exclude activity.id variable from the tidy set
+drop <- names(tidy.data) %in% c("activity.id") 
+tidy.data <- tidy.data[!drop]
 
-# Leave no trace
-rm(drop)
+if(leave.no.trace){
+  rm(drop)
+  rm(leave.no.trace)
+}
 
-# Create the tidy data set
-options(datatable.optimize=1)
-tidy <- data[, lapply(.SD, mean), by = activity]
+# Step 4. Export the Tidy Data Set
+
+# Export the tidy.data as a tab deliminated file
+write.table(tidy.data, "tidydata.txt", sep="\t")
